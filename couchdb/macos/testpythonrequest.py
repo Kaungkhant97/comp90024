a6c614b216087d6a2f97a0713b3e7be580082b4c
Kaung Khant Thu <kaungkhant.kt96@gmail.com>
2019-05-05 22:31:10 +1000

    proxy fixed

--- couchdb/macos/testpythonrequest.py
+++ couchdb/macos/testpythonrequest.py
@@ -1,10 +1,12 @@
 import requests
 
 URL = "http://user:pass@localhost:5984/_cluster_setup"
-my_proxies ={'http':'http://www.proxy.unimelb.edu.au:8000',
-'https':'https://www.proxy.unimelb.edu.au:8000'}
+my_proxies ={'http':'http://wwwproxy.unimelb.edu.au:8000',
+'https':'https://wwwproxy.unimelb.edu.au:8000'}
 r = requests.get(URL)
+print("content")
 print(r.content)
+print("proxy content")
 b = requests.get(URL,proxies=my_proxies)
 print(b.content)
 
